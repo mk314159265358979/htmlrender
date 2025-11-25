d12b958fb6907d72f7023e8ecc41b5a17a4a9085
mk314159265358979 <keram.koprowski@gmail.com>
2025-11-25 21:26:55 +0100

    Update anom.py

--- anom.py
+++ anom.py
@@ -1,21 +1,40 @@
 import re
-regexy = [r'\d{3}[- ]?\d{3}[- ]?\d{3}', r'\S*@\S*\.\S*', r'[A-ZĆŁŃŚŻŹ][a-zążźćęóńł]+ [A-ZĆŁŃŚŻŹ][a-zążźćęóńł]+', r'\d+[.,]\d{2}zł', r'\d{2}[.-]\d{2}[.-]\d{4}', r'\d{2}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}']
-nazwy = ["NRTELEFONU", "MAIL", "OSOBA", "KWOTA", "DATA", "NRKONTA"]
+from langdetect import detect
+from deep_translator import GoogleTranslator
+
+regexy = [
+    r'\d{3}[- ]?\d{3}[- ]?\d{3}',
+    r'\S*@\S*\.\S*',
+    r'[A-ZĆŁŃŚŻŹ][a-zążźćęóńł]+ [A-ZĆŁŃŚŻŹ][a-zążźćęóńł]+',
+    r'\d+[.,]\d{2}zł',
+    r'\d{2}[.-]\d{2}[.-]\d{4}',
+    r'\d{2}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}'
+]
+
+nazwy = ["NUMER TELEFONu", "MAIL", "OSOBA", "KWOTA", "DATA", "NUMER KONTA"]
+
+
 def anonimizacja(text, r=regexy, n=nazwy):
     for i in range(len(r)):
         wzorzec = r[i]
-        nazwa = n[i]
+        leng = detect(text)
+        nazwa = GoogleTranslator(source="pl", target=leng).translate(n[i])
         tnr = {}
-        i = 0  
+        i = 0
+
         def zamien(nr):
             nonlocal i
-            nr=re.sub("[- ]","",nr)
+            nr = re.sub("[- ]", "", nr)
             if nr not in tnr:
                 tnr[nr] = nazwa + f"{i}"
-                i+=1
+                i += 1
             return tnr[nr]
-        text = re.sub(wzorzec, lambda m: zamien(m.group(0)), text)   
+
+        text = re.sub(wzorzec, lambda m: zamien(m.group(0)), text)
+
     return text
 
-print("ALA ma kota")
-
+print(anonimizacja("Ala Nowak ma telefon numer 123456789 i jest to jej własny numer telefonu co ważne"))
+print(detect("To jest test po Polsku wykryj to"))
+print(detect("And this is in english"))
+print(GoogleTranslator(source="en", target="pl").translate("Hello world"))
